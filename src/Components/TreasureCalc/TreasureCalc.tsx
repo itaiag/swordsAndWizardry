f5336093b05115b28248051504633cb884a55adc
itaiag <itai.agmon@gmail.com>
2024-07-17 22:25:11 +0300

    Starting to work on treasure calc

--- src/Components/TreasureCalc/TreasureCalc.tsx
+++ src/Components/TreasureCalc/TreasureCalc.tsx
@@ -1,8 +1,21 @@
-import * as React from 'react';
+import React, { useState } from 'react';
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
+import { Button } from '@mui/material';
 
 export default function BasicTextFields() {
+    const [textFieldValue, setTextFieldValue] = useState('');
+    
+      // Step 2: Update the state on text field change
+      const handleTextFieldChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setTextFieldValue(event.target.value);
+    };
+
+    // Step 3: Add an onClick event handler to the button
+    const handleClick = () => {
+        console.log(textFieldValue); // This will print the value of the text field
+    };
+
     return (
         <Box
             component="form"
@@ -12,9 +25,14 @@ export default function BasicTextFields() {
             noValidate
             autoComplete="off"
         >
-            <TextField id="outlined-basic" label="Outlined" variant="outlined" />
-            <TextField id="filled-basic" label="Filled" variant="filled" />
-            <TextField id="standard-basic" label="Standard" variant="standard" />
+            <TextField 
+                id="standard-basic" 
+                label="נקודות נסיון" 
+                variant="standard"
+                value={textFieldValue} 
+                onChange={handleTextFieldChange}/>
+            <br/>
+            <Button variant="contained" onClick={handleClick}>חשב</Button>
         </Box>
     );
 }
